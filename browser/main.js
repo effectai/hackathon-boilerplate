c9d65d3c05201c35b0b313db53d0828d3e281ed6
David Britt <david@effect.ai>
2021-11-17 11:15:32 +0100

    Clean up

--- browser/main.js
+++ browser/main.js
@@ -1,12 +1,12 @@
-// import from node_modules
-// import Web3 from "web3";
 import "/node_modules/@effectai/effect-js/dist/index.umd.js"
 
 // import from unpkg
 // import { exports } from 'https://unpkg.com/@effectai/effect-js@0.0.31/dist/index.umd.js'
 
-
-// Figure out how to get jssig working, eosjs transit looks like to be the best solution at the moment to get it to work with an eosjs wallet.
+/**
+ * Experiments to get JsSignature provider to work:
+ * Figure out how to get jssig working, eosjs transit looks like to be the best solution at the moment to get it to work with an eosjs wallet.
+ */
 
 // npn install eosjs
 // import { JsSignatureProvider } from './node_modules/eosjs/dist/eosjs-jssig.js'
@@ -49,6 +49,9 @@ document.getElementById('btn-generate-client').onclick = () => {
     }        
 }
 
+/**
+ * Calling methods from the effectclient without connecting an account.
+ */
 document.getElementById('btn-get-campaign').onclick = async () => {
     const balanceReponse = await sdk.force.getPendingBalance();
     const campaignResponse = await sdk.force.getCampaign(5)
@@ -66,14 +69,16 @@ document.getElementById('btn-get-campaign').onclick = async () => {
 }
 
 /**
- * Create Effect Account
+ * BurnerWallet
  * Generates a new account when no parameter is passed.
  * Otherwise pass a web3 private key to create an account.
  */
 document.getElementById('btn-create-burner-wallet').onclick = () => {
     console.log('creating burner wallet...')
     try {
-        const burnerWallet = effectsdk.createAccount();
+        // When no parameters are passed to createAccount() a new keypair is generated.
+        const burnerWallet = effectsdk.createAccount( /* Insert Private key here */);
+
         burnerAccount = effectsdk.createWallet(burnerWallet);
         
         connectAccount.detail = burnerWallet
@@ -88,14 +93,14 @@ document.getElementById('btn-create-burner-wallet').onclick = () => {
 }
 
 /**
- * Create wallet
+ * Metamask
  * Generate web3 instance from account with private key.
  * Could also be the web3 object with a metamask connection. 
- * This examle uses Metamask.
  * 
  * Here we will also make a call to make sure we are on the correct chain.
- * Exhaustive list of available chains can be found here:
- * https://chainlist.org/
+ * Bsc-Mainnet: 0x38 (hex), 56 (decimal)
+ * Bsc-Testnet: 0x61 (hex), 97 (decimal)
+ * 
  */
 document.getElementById('btn-metamask').onclick =  async () => {
     console.log('Connecting to metamask wallet.')
@@ -106,9 +111,7 @@ document.getElementById('btn-metamask').onclick =  async () => {
                 method: 'wallet_switchEthereumChain',
                 params: [{ chainId: '0x38' }] // 0x38 is the chainId of bsc testnet.    
             }) 
-    
-            // const metamaskAccount = new effectsdk.createWallet(ethAccount)
-            
+                
             connectAccount.detail = ethAccount
             connectAccount.address = ethAccount[0]
             connectAccount.accountService = 'metamask'
@@ -128,7 +131,21 @@ document.getElementById('btn-metamask').onclick =  async () => {
 }
 
 /**
- * Connect Account
+ * TODO
+ * Anchor Wallet
+ */
+document.getElementById('btn-anchor').onclick = () => {
+    console.log('generating key...')
+
+    try {
+        // Do stuff
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+/**
+ * Connect to Effect Account using burnerwallet, metamask
  */
 document.getElementById('btn-connect-account').onclick = async () => {
     console.log('Connecting to account with wallet.')
@@ -140,7 +157,8 @@ document.getElementById('btn-connect-account').onclick = async () => {
             const metaWeb3 = new Web3(window.ethereum)
             connectReponse = await sdk.connectAccount(metaWeb3)
         } else {
-            connectReponse = await sdk.connectAccount()
+            // connectReponse = await sdk.connectAccount()
+            alert('Login with on of the wallet.')
         }
         document.getElementById('connect-account').innerHTML = `<p>${JSON.stringify(connectReponse, null, 2)}</p>`
     } catch (error) {
@@ -149,6 +167,7 @@ document.getElementById('btn-connect-account').onclick = async () => {
 }
 
 /**
+ * DEBUG
  * Make Campaign
  * We need to upload the campaign to IPFS, then create the campaign on the blockchain.
  * This is done for us by the makeCampaign function.
@@ -156,58 +175,83 @@ document.getElementById('btn-connect-account').onclick = async () => {
 document.getElementById('btn-make-campaign').onclick = async () => {
     console.log('creating campaign...')
     
-    // Template String is available in this repo, otherwise it can be anywhere else.
-    const templateResponse = await fetch('./template.html')
-    const templateText = await templateResponse.text()
-
-    const campaignToIpfs = {
-        title: 'SpaceX Satelite Identifier.',
-        description: 'Networked well-modulated instruction set',
-        instructions: `American whole magazine truth stop whose. On traditional measure example sense peace`,
-        template: templateText,
-        image: 'https://ipfs.effect.ai/ipfs/bafkreiggnttdaxleeii6cdt23i4e24pfcvzyrndf5kzfbqgf3fxjryj5s4',
-        category: 'Image Labeling',
-        example_task: {'image_url': 'https://ipfs.effect.ai/ipfs/bafkreidrxwhqsxa22uyjamz7qq3lh7pv2eg3ykodju6n7cgprmjpal2oga'},
-        version: 1,
-        reward: 1
-    }
-
-    const quantity = 1; // How much EFX is rewarded for each task.
-    const makeCampaingResponse = await sdk.force.makeCampaign(campaignToIpfs, quantity)
-    console.log(makeCampaingResponse)
-
-    const divShowCampaign = document.getElementById('show-campaign');
-
-
-    // // Upload campaign to IPFS
-    // divUploadCampaign.innerHTML = `<p>Uploading Campaign to IPFS...</p>`
-    // const uploadResponse = await sdk.force.uploadCampaing(campaignToIpfs)
-    // console.log(uploadResponse)
-    // divUploadCampaign.innerHTML = `<p>Uploaded Campaign to IPFS!</p>`
-    // divUploadCampaign.innerHTML += `<p>${JSON.stringify(uploadResponse, null, 2)}</p>`
-
-    // // Create campaign on the blockchain
-    // const divCreateCampaign = document.getElementById('create-campaign');
-    // divCreateCampaign.innerHTML = `<p>Creating Campaign on Blockchain...</p>`
-    // const createResponse = await sdk.force.createCampaign(uploadResponse.hash)
-    // console.log(createResponse)
-    // divCreateCampaign.innerHTML = `<p>Created Campaign on Blockchain!</p>`
-    // divCreateCampaign.innerHTML += `<p>${JSON.stringify(createResponse, null, 2)}</p>`
-
     try {
-        
+        // Template String is available in this repo, otherwise it can be anywhere else.
+        const templateResponse = await fetch('./template.html')
+        const templateText = await templateResponse.text()
+
+        const campaignToIpfs = {
+            title: 'SpaceX Satelite Identifier.',
+            description: 'Networked well-modulated instruction set',
+            instructions: `American whole magazine truth stop whose. On traditional measure example sense peace`,
+            template: templateText,
+            image: 'https://ipfs.effect.ai/ipfs/bafkreiggnttdaxleeii6cdt23i4e24pfcvzyrndf5kzfbqgf3fxjryj5s4',
+            category: 'Image Labeling',
+            example_task: {'image_url': 'https://ipfs.effect.ai/ipfs/bafkreidrxwhqsxa22uyjamz7qq3lh7pv2eg3ykodju6n7cgprmjpal2oga'},
+            version: 1,
+            reward: 1
+        }
+
+        const quantity = 1; // How much EFX is rewarded for each task.
+        const makeCampaingResponse = await sdk.force.makeCampaign(campaignToIpfs, quantity)
+        console.log(makeCampaingResponse)
+
+        const divShowCampaign = document.getElementById('show-campaign');
+        divShowCampaign.innerHTML = `<p>${JSON.stringify(makeCampaingResponse, null, 2)}</p>`        
     } catch (error) {
         console.error(error)
     }
 }
 
-// document.getElementById('btn-key').onclick = () => {
-//     console.log('generating key...')
+/**
+ * TODO
+ * Make Batch
+ */
+document.getElementById('btn-key').onclick = () => {
+    console.log('generating key...')
+    try {
+        const campaignId = 88
+        const campaign = await sdk.force.getCampaign(campaignId)
+        console.log('Campaign', campaign)
+    
+        // Get Campaign Batches.
+        const batches = await sdk.force.getCampaignBatches(campaignId)
+        console.log(`Batches for campaign ${campaignId}`, batches)
+    
+        const content = {
+            'tasks': [
+                {"ipfs": "bafkreiggnttdaxleeii6cdt23i4e24pfcvzyrndf5kzfbqgf3fxjryj5s4"}, 
+                {"ipfs": "bafkreidrxwhqsxa22uyjamz7qq3lh7pv2eg3ykodju6n7cgprmjpal2oga"}, 
+                {"ipfs": "bafkreid2ocabg7mo235uuwactlcf7vzxyagoxeroyrubfufwobtqz3q27q"}, 
+                {"ipfs": "bafkreifu5xciyxpwnmkorzddanqtc66i43q5cn4sdkb3l563yjzd7s3274"}
+            ]
+        }
+       
+        const repetitions = 1
+        // Create batch for campaign.
+        // same here as for campaign, id of batch needs to be returned
+        const batch = await sdk.force.createBatch(campaign.id, batches.length, content, repetitions)
 
-//     try {
+        console.log(batch);
 
-//     } catch (error) {
-//         console.error(error)
-//     }
-// }
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+/**
+ * TODO
+ * Results
+ */
+document.getElementById('btn-key').onclick = () => {
+    console.log('generating key...')
+    try {
+        
+        // Get task submissions of batch.
+        const taskResults = await sdk.force.getTaskSubmissionsForBatch()
+        console.log('taskResults for new batch', taskResults)
+    } catch (error) {
+        console.error(error)
+    }
+}
 
